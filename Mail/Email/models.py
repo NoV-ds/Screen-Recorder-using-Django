7450344709c966974817be87fc43623f77bf095b
vikas <takvikas259@gmail.com>
2021-09-24 17:30:03 +0530

    modified existing files

--- Mail/Email/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class UserDetails(models.Model):
-    fullname = models.CharField(max_length=50, verbose_name="Full Name")
-    password = models.CharField(max_length=50, verbose_name="Password")
-    email    = models.EmailField(max_length=254, verbose_name="Email")
--- ScreenRecorder/recording/Video_Record.py
+++ ScreenRecorder/recording/Video_Record.py
@@ -17,18 +17,25 @@ def ScreenSize():
     size = (width,height)
     return size
 
-def Screen_Recorder(out_video):
-    for i in range(480):
-        img = pyt.screenshot()
-        frame = np.array(img)
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        out_video.write(frame)
-    cv.destroyAllWindows()
-    return out_video.release()
+def Screen_Recorder(out_video, start_rec):
+    if start_rec == "start":
+        while True:
+            img = pyt.screenshot()
+            frame = np.array(img)
+            frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            out_video.write(frame)
+            cv.imshow("Recording in progress ...", frame)
+            if cv.waitKey(1)==ord("q"):
+                break
+        cv.destroyAllWindows()
+        return out_video.release()
+    else:
+        exit(0)
 
 if __name__=="__main__":
     fourcc = cv.VideoWriter_fourcc(*"XVID")
+    start_rec = str(input())
     path = "../media/Recording"+str(id_generator())+".mp4"
     print(path)
     out_video = cv.VideoWriter(path, fourcc, 24.0, ScreenSize())
-    Screen_Recorder(out_video)
+    Screen_Recorder(out_video, start_rec)
